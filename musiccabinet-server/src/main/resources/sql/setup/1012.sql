ea09c2339c7e6a34056bd5f2243f689d2c539743
Hakan Eriksson <hakko@dilerium.se>
2012-06-22 19:41:02 +0200

    Added sql script to clean up possible existence of duplicate artist rows

---
+++ musiccabinet-server/src/main/resources/sql/setup/1012.sql
@@ -0,0 +1,93 @@
+create table music.duplicate_artists (artist_id integer);
+
+insert into music.duplicate_artists (artist_id)
+select id from music.artist where artist_name in (
+	select artist_name from music.artist 
+	group by artist_name having count(artist_name) > 1
+);
+
+delete from library.artisttoptrackplaycount where artist_id in (
+	select artist_id from music.duplicate_artists
+);
+	
+delete from library.musicdirectory_import;
+delete from library.musicdirectory where artist_id in (
+	select artist_id from music.duplicate_artists
+);
+
+delete from library.musicfile_import;
+delete from library.musicfile where track_id in (
+	select id from music.track where artist_id in (
+		select artist_id from music.duplicate_artists
+	)
+);
+
+delete from library.trackplaycount_import;
+delete from library.trackplaycount where track_id in (
+	select id from music.track where artist_id in (
+		select artist_id from music.duplicate_artists
+	)
+);
+
+delete from library.usertopartist_import;
+delete from library.usertopartist where artist_id in (
+	select artist_id from music.duplicate_artists
+);
+
+delete from library.webservice_history where artist_id in (
+	select artist_id from music.duplicate_artists
+);
+
+delete from library.webservice_history where album_id in (
+	select id from music.album where artist_id in (
+		select artist_id from music.duplicate_artists
+	)
+);
+
+delete from music.albuminfo_import;
+delete from music.artistinfo_import;
+delete from music.artistrelation_import;
+delete from music.artisttoptag_import;
+delete from music.artisttoptrack_import;
+delete from music.trackrelation_import;
+
+delete from music.albuminfo where album_id in (
+	select id from music.album where artist_id in (
+		select artist_id from music.duplicate_artists
+	)
+);
+
+delete from music.artistinfo where artist_id in (
+	select artist_id from music.duplicate_artists
+);
+
+delete from music.artistrelation where source_id in (
+	select artist_id from music.duplicate_artists
+);
+delete from music.artistrelation where target_id in (
+	select artist_id from music.duplicate_artists
+);
+
+delete from music.artisttoptag where artist_id in (
+	select artist_id from music.duplicate_artists
+);
+
+delete from music.artisttoptrack where artist_id in (
+	select artist_id from music.duplicate_artists
+);
+
+delete from music.album where artist_id in (
+	select artist_id from music.duplicate_artists
+);
+
+delete from music.track where artist_id in (
+	select artist_id from music.duplicate_artists
+);
+
+delete from music.artist where id in (
+	select artist_id from music.duplicate_artists
+);
+
+drop table music.duplicate_artists;
+
+create unique index artist_artistname on music.artist (artist_name);
