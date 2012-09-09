a3ac3eecd657fd33b85dc449458fea6c2718f631
Hakan Eriksson <hakko@dilerium.se>
2012-09-09 13:26:14 +0200

    Make drop utility schema aware

--- musiccabinet-server/src/main/resources/sql/util/drop-function.sql
+++ musiccabinet-server/src/main/resources/sql/util/drop-function.sql
@@ -5,13 +5,15 @@ begin
 
 	if exists (select 1 from pg_proc proc 
 		inner join pg_language lang on proc.prolang = lang.oid 
-		where lang.lanname = 'plpgsql' and proname = function_name) then
+		inner join pg_namespace ns on proc.pronamespace = ns.oid
+		where lang.lanname = 'plpgsql' and proname = function_name and nspname = schema) then
 
 		select 'drop function ' || schema || '.' || proname || 
 		'(' || oidvectortypes(proargtypes) || ');'
 		into dropStatement from pg_proc proc
 		inner join pg_language lang on proc.prolang = lang.oid 
-		where lang.lanname = 'plpgsql' and proname = function_name;
+		inner join pg_namespace ns on proc.pronamespace = ns.oid
+		where lang.lanname = 'plpgsql' and proname = function_name and nspname = schema;
 		
 		execute dropStatement;
 	
