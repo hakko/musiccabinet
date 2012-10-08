3a23bc150aec0d289e6a32e9ce0a92e10884d5ba
Hakan Eriksson <hakko@dilerium.se>
2012-10-08 13:29:24 +0200

    Bug fix: only drop function on previous installs

--- musiccabinet-server/src/main/resources/sql/setup/1028.sql
+++ musiccabinet-server/src/main/resources/sql/setup/1028.sql
@@ -6,6 +6,6 @@ alter table library.usertopartist_import set schema music;
 alter table library.userrecommendedartist set schema music;
 alter table library.userrecommendedartist_import set schema music;
 
-drop function library.update_usertopartists();
-drop function library.update_userrecartists();
-drop function library.get_lastfmuser_id(text);		
+drop function if exists library.update_usertopartists();
+drop function if exists library.update_userrecartists();
+drop function if exists library.get_lastfmuser_id(text);		
