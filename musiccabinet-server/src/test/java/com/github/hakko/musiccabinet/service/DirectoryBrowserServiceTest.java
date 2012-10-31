1c9d643194e9167224343f85a66324d7b6b2ecc0
Hakan Eriksson <hakko@dilerium.se>
2012-10-31 20:32:34 +0100

    Cleanup: remove unused import

--- musiccabinet-server/src/test/java/com/github/hakko/musiccabinet/service/DirectoryBrowserServiceTest.java
+++ musiccabinet-server/src/test/java/com/github/hakko/musiccabinet/service/DirectoryBrowserServiceTest.java
@@ -20,7 +20,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.github.hakko.musiccabinet.dao.jdbc.JdbcDirectoryBrowserDao;
-import com.github.hakko.musiccabinet.dao.jdbc.JdbcMusicDao;
 import com.github.hakko.musiccabinet.dao.util.PostgreSQLUtil;
 import com.github.hakko.musiccabinet.domain.model.aggr.DirectoryContent;
 import com.github.hakko.musiccabinet.domain.model.library.Directory;
