9b7f3329b08cd1b6f355dc403619bd303d20ed85
Hakan Eriksson <hakko@dilerium.se>
2012-11-11 17:05:59 +0100

    Include empty update, to force reading of block_webservice function

--- musiccabinet-server/src/test/java/com/github/hakko/musiccabinet/service/DatabaseAdministrationServiceTest.java
+++ musiccabinet-server/src/test/java/com/github/hakko/musiccabinet/service/DatabaseAdministrationServiceTest.java
@@ -26,7 +26,7 @@ public class DatabaseAdministrationServiceTest {
 	public void serviceFindsAllDatabaseUpdates() {
 		List<Integer> updates = dbAdmService.getDatabaseUpdates();
 
-		final int NR_OF_UPDATES = 30;
+		final int NR_OF_UPDATES = 31;
 		
 		Assert.assertNotNull(updates);
 		Assert.assertEquals(NR_OF_UPDATES, updates.size());
