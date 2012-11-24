89cb4ce23b1a63a22a8b4198c8402981499cff1f
Hakan Eriksson <hakko@dilerium.se>
2012-11-24 19:13:51 +0100

    Add id explicitly (serial is 1-based)

--- musiccabinet-server/src/main/resources/sql/setup/1031.sql
+++ musiccabinet-server/src/main/resources/sql/setup/1031.sql
@@ -1,15 +1,15 @@
-create table music.mb_album_type (id serial primary key, description text not null);
-insert into music.mb_album_type (description) values ('single');
-insert into music.mb_album_type (description) values ('ep');
-insert into music.mb_album_type (description) values ('album');
-insert into music.mb_album_type (description) values ('compilation');
-insert into music.mb_album_type (description) values ('soundtrack');
-insert into music.mb_album_type (description) values ('spokenword');
-insert into music.mb_album_type (description) values ('interview');
-insert into music.mb_album_type (description) values ('audiobook');
-insert into music.mb_album_type (description) values ('live');
-insert into music.mb_album_type (description) values ('remix');
-insert into music.mb_album_type (description) values ('other');
+create table music.mb_album_type (id integer primary key, description text not null);
+insert into music.mb_album_type (id, description) values (0, 'single');
+insert into music.mb_album_type (id, description) values (1, 'ep');
+insert into music.mb_album_type (id, description) values (2, 'album');
+insert into music.mb_album_type (id, description) values (3, 'compilation');
+insert into music.mb_album_type (id, description) values (4, 'soundtrack');
+insert into music.mb_album_type (id, description) values (5, 'spokenword');
+insert into music.mb_album_type (id, description) values (6, 'interview');
+insert into music.mb_album_type (id, description) values (7, 'audiobook');
+insert into music.mb_album_type (id, description) values (8, 'live');
+insert into music.mb_album_type (id, description) values (9, 'remix');
+insert into music.mb_album_type (id, description) values (10, 'other');
 
 create table music.mb_artist (artist_id integer references music.artist (id) not null, mbid char(36) not null, country_code char(2), start_year smallint, active boolean);
 create table music.mb_album (album_id integer references music.album (id) not null, mbid char(36) not null, type_id integer references music.mb_album_type (id) not null, release_year smallint);
