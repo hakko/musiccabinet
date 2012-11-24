67637df9b167d238a3b561a8f879ea2c2bbf651e
Hakan Eriksson <hakko@dilerium.se>
2012-11-24 20:24:31 +0100

    Fix: namespace is present

--- musiccabinet-server/src/main/java/com/github/hakko/musiccabinet/parser/musicbrainz/ArtistQueryParserImpl.java
+++ musiccabinet-server/src/main/java/com/github/hakko/musiccabinet/parser/musicbrainz/ArtistQueryParserImpl.java
@@ -2,11 +2,6 @@ package com.github.hakko.musiccabinet.parser.musicbrainz;
 
 import java.io.InputStream;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-
-import org.xml.sax.SAXException;
-
 import com.github.hakko.musiccabinet.domain.model.music.MBArtist;
 import com.github.hakko.musiccabinet.exception.ApplicationException;
 import com.github.hakko.musiccabinet.parser.AbstractSAXParserImpl;
@@ -24,17 +19,6 @@ public class ArtistQueryParserImpl extends AbstractSAXParserImpl implements
 	@Override
 	public MBArtist getArtist() {
 		return handler.getArtist();
-	}
-
-	// the XML returned from MusicBrainz is invalid,
-	// attribute ext:score has no declared namespace.
-	// turn off namespace validation to make it pass.
-	protected synchronized SAXParser getSAXParser()
-			throws ParserConfigurationException, SAXException {
-		SAXParser saxParser = super.getSAXParser();
-		saxParser.getXMLReader().setFeature(
-				"http://xml.org/sax/features/namespaces", false);
-		return saxParser;
 	}
 
 }
