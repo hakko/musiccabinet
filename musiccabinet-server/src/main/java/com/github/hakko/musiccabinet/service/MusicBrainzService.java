707efd922e06398cc96aa55a17f8b542d6c6f214
Hakan Eriksson <hakko@dilerium.se>
2012-11-24 20:12:59 +0100

    MusicBrainz service

---
+++ musiccabinet-server/src/main/java/com/github/hakko/musiccabinet/service/MusicBrainzService.java
@@ -0,0 +1,146 @@
+package com.github.hakko.musiccabinet.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.hakko.musiccabinet.dao.MusicBrainzAlbumDao;
+import com.github.hakko.musiccabinet.dao.MusicBrainzArtistDao;
+import com.github.hakko.musiccabinet.domain.model.music.Artist;
+import com.github.hakko.musiccabinet.domain.model.music.MBAlbum;
+import com.github.hakko.musiccabinet.domain.model.music.MBArtist;
+import com.github.hakko.musiccabinet.exception.ApplicationException;
+import com.github.hakko.musiccabinet.log.Logger;
+import com.github.hakko.musiccabinet.parser.musicbrainz.ArtistQueryParser;
+import com.github.hakko.musiccabinet.parser.musicbrainz.ArtistQueryParserImpl;
+import com.github.hakko.musiccabinet.parser.musicbrainz.ReleaseGroupParser;
+import com.github.hakko.musiccabinet.parser.musicbrainz.ReleaseGroupParserImpl;
+import com.github.hakko.musiccabinet.util.StringUtil;
+import com.github.hakko.musiccabinet.ws.musicbrainz.ArtistQueryClient;
+import com.github.hakko.musiccabinet.ws.musicbrainz.ReleaseGroupsClient;
+
+public class MusicBrainzService {
+
+	protected MusicBrainzArtistDao artistDao;
+	protected MusicBrainzAlbumDao albumDao;
+
+	protected ArtistQueryClient artistQueryClient;
+	protected ReleaseGroupsClient releaseGroupsClient;
+	
+	protected boolean isIndexBeingCreated = false;
+	protected int mbid, mbids, discography, discographies;
+	
+	private static final Logger LOG = Logger.getLogger(MusicBrainzService.class);
+	
+	public static final int TYPE_SINGLE =		1 << 0;
+	public static final int TYPE_EP =			1 << 1;
+	public static final int TYPE_ALBUM =		1 << 2;
+	public static final int TYPE_COMPILATION =	1 << 3;
+	public static final int TYPE_SOUNDTRACK =	1 << 4;
+	public static final int TYPE_SPOKENWORD =	1 << 5;
+	public static final int TYPE_INTERVIEW =	1 << 6;
+	public static final int TYPE_AUDIOBOOK =	1 << 7;
+	public static final int TYPE_LIVE =			1 << 8;
+	public static final int TYPE_REMIX =		1 << 9;
+	public static final int TYPE_OTHER =		1 << 10;
+	
+	public boolean isIndexBeingCreated() {
+		return isIndexBeingCreated;
+	}
+	
+	public String getProgressDescription() {
+		return String.format("%d/%d artist ids, %d/%d artist discographies", 
+				mbid, mbids, discography, discographies);
+	}
+	
+	public int getMissingAndOutdatedArtistsCount() {
+		return artistDao.getMissingAndOutdatedArtistsCount();
+	}
+	
+	public boolean hasDiscography() {
+		return albumDao.hasDiscography();
+	}
+	
+	public List<MBAlbum> getMissingAlbums(String artistName, int typeMask,
+			String lastFmUsername, int playedWithinLastDays, int offset) {
+		return albumDao.getMissingAlbums(artistName, typeMask,
+				lastFmUsername, playedWithinLastDays, offset);
+	}
+
+	public void updateDiscography() {
+		isIndexBeingCreated = true;
+		mbid = discography = discographies = 0;
+		updateArtistIds();
+		updateArtistDiscographies();
+		isIndexBeingCreated = false;
+	}
+	
+	protected void updateArtistIds() {
+		List<Artist> missingArtists = artistDao.getMissingArtists();
+		List<MBArtist> mbArtists = new ArrayList<>();
+		mbids = missingArtists.size();
+		for (Artist artist : artistDao.getMissingArtists()) {
+			try {
+				StringUtil response = new StringUtil(artistQueryClient.get(artist.getName()));
+				ArtistQueryParser parser = new ArtistQueryParserImpl(response.getInputStream());
+				mbArtists.add(parser.getArtist());
+				if (mbArtists.size() > 100) {
+					artistDao.createArtists(mbArtists);
+					mbArtists.clear();
+				}
+				++mbid;
+			} catch (ApplicationException e) {
+				LOG.warn("Couldn't read mbid for " + artist.getName(), e);
+			}
+		}
+		artistDao.createArtists(mbArtists);
+	}
+	
+	protected void updateArtistDiscographies() {
+		List<MBArtist> outdatedArtists = artistDao.getOutdatedArtists();
+		List<MBAlbum> mbAlbums = new ArrayList<>();
+		ReleaseGroupParser parser;
+		discographies = outdatedArtists.size();
+		for (MBArtist artist : outdatedArtists) {
+			try {
+				int offset = 0;
+				do { 
+					StringUtil response = new StringUtil(releaseGroupsClient.get(
+						artist.getName(), artist.getMbid(), offset));
+					parser = new ReleaseGroupParserImpl(response.getInputStream());
+					for (MBAlbum album : parser.getAlbums()) {
+						album.setArtist(artist.asArtist());
+					}
+					mbAlbums.addAll(parser.getAlbums());
+					offset += 100;
+				} while (offset < parser.getTotalAlbums());
+				++discography;
+				if (mbAlbums.size() > 1000) {
+					albumDao.createAlbums(mbAlbums);
+					mbAlbums.clear();
+				}
+			} catch (ApplicationException e) {
+				LOG.warn("Couldn't read discography for " + artist.getName(), e);
+			}
+		}
+		albumDao.createAlbums(mbAlbums);
+	}
+	
+	// Spring setters
+	
+	public void setMusicBrainzArtistDao(MusicBrainzArtistDao artistDao) {
+		this.artistDao = artistDao;
+	}
+
+	public void setMusicBrainzAlbumDao(MusicBrainzAlbumDao albumDao) {
+		this.albumDao = albumDao;
+	}
+
+	public void setArtistQueryClient(ArtistQueryClient artistQueryClient) {
+		this.artistQueryClient = artistQueryClient;
+	}
+
+	public void setReleaseGroupsClient(ReleaseGroupsClient releaseGroupsClient) {
+		this.releaseGroupsClient = releaseGroupsClient;
+	}
+	
+}
---
+++ musiccabinet-server/src/test/java/com/github/hakko/musiccabinet/service/MusicBrainzServiceTest.java
@@ -0,0 +1,94 @@
+package com.github.hakko.musiccabinet.service;
+
+import static com.github.hakko.musiccabinet.service.MusicBrainzService.TYPE_ALBUM;
+import static com.github.hakko.musiccabinet.util.UnittestLibraryUtil.getFile;
+import static com.github.hakko.musiccabinet.util.UnittestLibraryUtil.submitFile;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.github.hakko.musiccabinet.dao.jdbc.JdbcLibraryAdditionDao;
+import com.github.hakko.musiccabinet.domain.model.music.MBAlbum;
+import com.github.hakko.musiccabinet.exception.ApplicationException;
+import com.github.hakko.musiccabinet.util.ResourceUtil;
+import com.github.hakko.musiccabinet.ws.musicbrainz.ArtistQueryClient;
+import com.github.hakko.musiccabinet.ws.musicbrainz.ReleaseGroupsClient;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+public class MusicBrainzServiceTest {
+
+	@Autowired
+	private MusicBrainzService service;
+
+	@Autowired
+	private JdbcLibraryAdditionDao additionDao;
+	
+	private String artistName = "Cult of Luna", albumName = "Eternal Kingdom", 
+			trackName = "Owlwood", mbid = "d347406f-839d-4423-9a28-188939282afa";
+	
+	@Before
+	public void configureService() throws ApplicationException {
+		service.setArtistQueryClient(getArtistQueryClient());
+		service.setReleaseGroupsClient(getReleaseGroupsClient());
+	}
+	
+	@Before
+	public void prepareTestdata() {
+		additionDao.getJdbcTemplate().execute("truncate music.artist cascade");
+		additionDao.getJdbcTemplate().execute("truncate library.file cascade");
+
+		submitFile(additionDao, getFile(artistName, albumName, trackName));
+	}
+	
+	@Test
+	public void updatesDiscography() {
+		assertFalse(service.isIndexBeingCreated());
+		Assert.assertEquals("0/0 artist ids, 0/0 artist discographies",
+				service.getProgressDescription());
+		
+		service.updateDiscography();
+		
+		assertFalse(service.isIndexBeingCreated());
+		Assert.assertEquals("1/1 artist ids, 1/1 artist discographies",
+				service.getProgressDescription());
+		
+		List<MBAlbum> albums = service.getMissingAlbums(artistName, TYPE_ALBUM, null, -1, 0);
+		Assert.assertEquals(4, albums.size());
+		
+		assertEquals("Cult of Luna", albums.get(0).getTitle());
+		assertEquals("The Beyond", albums.get(1).getTitle());
+		assertEquals("Salvation", albums.get(2).getTitle());
+		assertEquals("Somewhere Along the Highway", albums.get(3).getTitle());
+		// Eternal Kingdom exists in library
+	}
+	
+	private ArtistQueryClient getArtistQueryClient() throws ApplicationException {
+		ArtistQueryClient client = Mockito.mock(ArtistQueryClient.class);
+		
+		Mockito.when(client.get(artistName)).thenReturn(
+				new ResourceUtil("musicbrainz/xml/artistQuery.xml").getContent());
+			
+		return client;
+	}
+
+	private ReleaseGroupsClient getReleaseGroupsClient() throws ApplicationException {
+		ReleaseGroupsClient client = Mockito.mock(ReleaseGroupsClient.class);
+		
+		Mockito.when(client.get(artistName, mbid, 0)).thenReturn(
+				new ResourceUtil("musicbrainz/xml/releaseGroup.xml").getContent());
+		
+		return client;
+	}
+
+}
