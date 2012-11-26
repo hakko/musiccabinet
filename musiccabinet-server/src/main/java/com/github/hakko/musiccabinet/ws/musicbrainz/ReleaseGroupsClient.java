8b91fbafdb1902605a3b830f4f021ad8ee77e648
Hakan Eriksson <hakko@dilerium.se>
2012-11-26 21:04:06 +0100

    Exclude non studio albums

--- musiccabinet-server/src/main/java/com/github/hakko/musiccabinet/ws/musicbrainz/ReleaseGroupsClient.java
+++ musiccabinet-server/src/main/java/com/github/hakko/musiccabinet/ws/musicbrainz/ReleaseGroupsClient.java
@@ -1,6 +1,7 @@
 package com.github.hakko.musiccabinet.ws.musicbrainz;
 
 import static com.github.hakko.musiccabinet.domain.model.library.WebserviceInvocation.Calltype.MB_RELEASE_GROUPS;
+import static java.lang.String.format;
 import static java.lang.String.valueOf;
 
 import java.util.ArrayList;
@@ -15,13 +16,26 @@ import com.github.hakko.musiccabinet.exception.ApplicationException;
 
 /*
  * Executes a request to fetch MusicBrainz releases for an artist, i.e:
- * http://www.musicbrainz.org/ws/2/release-group/?artist=5b11f4ce-a62d-471e-81fc-a69a8278c7da&limit=100
+ * http://musicbrainz.org/ws/2/release-group/?query=arid:5b11f4ce-a62d-471e-81fc-a69a8278c7da AND (primarytype:Album OR primarytype:EP OR primarytype:Single) AND NOT secondarytype:compilation AND NOT secondarytype:live AND NOT secondarytype:soundtrack AND status:official&limit=100
+ * 
+ * Doc: http://musicbrainz.org/doc/Indexed_Search_Syntax 
  */
 public class ReleaseGroupsClient extends AbstractMusicBrainzClient {
 
 	protected static final String PATH = "/ws/2/release-group/";
 	
-	protected static final String ARTIST = "artist";
+	protected static final String QUERY = "query";
+	protected static final String PATTERN = "arid:%s"
+			+ " AND (primarytype:Album OR primarytype:EP OR primarytype:Single)"
+			+ " AND NOT secondarytype:compilation"
+			+ " AND NOT secondarytype:soundtrack"
+			+ " AND NOT secondarytype:spokenword"
+			+ " AND NOT secondarytype:interview"
+			+ " AND NOT secondarytype:audiobook"
+			+ " AND NOT secondarytype:live"
+			+ " AND NOT secondarytype:remix"
+			+ " AND NOT secondarytype:other"
+			+ " AND status:official";
 	
 	protected static final String LIMIT = "limit";
 	protected static final String HUNDRED = "100";
@@ -32,7 +46,7 @@ public class ReleaseGroupsClient extends AbstractMusicBrainzClient {
 		WebserviceInvocation invocation = new WebserviceInvocation(
 				MB_RELEASE_GROUPS, new Artist(artistName));
 		List<NameValuePair> params = new ArrayList<>();
-		params.add(new BasicNameValuePair(ARTIST, mbid));
+		params.add(new BasicNameValuePair(QUERY, format(PATTERN, mbid)));
 		params.add(new BasicNameValuePair(LIMIT, HUNDRED));
 		params.add(new BasicNameValuePair(OFFSET, valueOf(offset)));
 		return executeWSRequest(invocation, PATH, params);
--- musiccabinet-server/src/test/java/com/github/hakko/musiccabinet/ws/musicbrainz/ReleaseGroupsClientTest.java
+++ musiccabinet-server/src/test/java/com/github/hakko/musiccabinet/ws/musicbrainz/ReleaseGroupsClientTest.java
@@ -22,7 +22,19 @@ public class ReleaseGroupsClientTest {
 		ReleaseGroupsClient releaseGroupsClient = getReleaseGroupsClient();
 
 		final String ARTIST_NAME = "Cult of Luna", MBID = "d347406f-839d-4423-9a28-188939282afa",
-				EXPECTED_URI = "http://musicbrainz.org/ws/2/release-group/?artist=d347406f-839d-4423-9a28-188939282afa&limit=100&offset=0";
+				EXPECTED_URI = "http://musicbrainz.org/ws/2/release-group/?"
+				+ "query=arid%3Ad347406f-839d-4423-9a28-188939282afa"
+				+ "+AND+%28primarytype%3AAlbum+OR+primarytype%3AEP+OR+primarytype%3ASingle%29"
+				+ "+AND+NOT+secondarytype%3Acompilation"
+				+ "+AND+NOT+secondarytype%3Asoundtrack"
+				+ "+AND+NOT+secondarytype%3Aspokenword"
+				+ "+AND+NOT+secondarytype%3Ainterview"
+				+ "+AND+NOT+secondarytype%3Aaudiobook"
+				+ "+AND+NOT+secondarytype%3Alive"
+				+ "+AND+NOT+secondarytype%3Aremix"
+				+ "+AND+NOT+secondarytype%3Aother"
+				+ "+AND+status%3Aofficial"
+				+ "&limit=100&offset=0";
 
 		ArgumentCaptor<HttpGet> argument = forClass(HttpGet.class);
 		releaseGroupsClient.get(ARTIST_NAME, MBID, 0);
